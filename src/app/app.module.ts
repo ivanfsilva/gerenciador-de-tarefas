6a514760f8ccf1d1ecda51003503d60b921059c9
ivanfsilva <ivanfs27@gmail.com>
2021-02-01 16:33:26 -0300

    criando modulo tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { TarefasModule } from './tarefas';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
